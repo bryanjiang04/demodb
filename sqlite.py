7ae39f73cfd6ba6bf5a7342fffca919cfa1b87a7
Bryan Jiang <bryanjiang04@gmail.com>
2024-11-13 23:45:56 -0500

    11/13/24 edits

---
+++ sqlite.py
@@ -0,0 +1,5 @@
+import sqlite3 as sql
+
+conn = sql.connect('webscrape.py')
+
+c = conn.cursor()
---
+++ webscrape.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+
+soup = BeautifulSoup(r.content,'html.parser')
+s = soup.find('div', class_='entry-content')
+
+content = s.find_all('p')
+print(content)
